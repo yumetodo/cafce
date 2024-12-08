3cd982747a2afe11dafeb9d68e91f15d8a4bd9a3
yumetodo <yume-wikijp@live.jp>
2024-12-08 22:47:33 +0900

    feat: コマンドライン引数のパースを実装

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,47 @@
+mod env;
 mod setting;
+use bpaf::*;
+use std::path::PathBuf;
+
+#[derive(Debug, Clone, Bpaf)]
+#[allow(dead_code)]
+#[bpaf(options)]
+struct Opts {
+    #[bpaf(external)]
+    action: Action,
+}
+
+#[derive(Debug, Clone, Bpaf)]
+enum Action {
+    #[bpaf(command)]
+    Store { config: PathBuf },
+
+    #[bpaf(command)]
+    Restore { config: PathBuf },
+
+    #[bpaf(command)]
+    Init { config: PathBuf },
+}
 
 fn main() {
-    println!("Hello, world!");
+    let ops = opts().run();
+    match ops.action {
+        Action::Init { config } => {
+            setting::Setting::init_to_file(&config).unwrap();
+        }
+        Action::Store { config } => {
+            let environment = env::Env::new().unwrap();
+            let setting = setting::Setting::new_from_file(&config).unwrap();
+            println!("{:#?}", config);
+            println!("{:#?}", environment);
+            println!("{:#?}", setting);
+        }
+        Action::Restore { config } => {
+            let environment = env::Env::new().unwrap();
+            let setting = setting::Setting::new_from_file(&config).unwrap();
+            println!("{:#?}", config);
+            println!("{:#?}", environment);
+            println!("{:#?}", setting);
+        }
+    }
 }
